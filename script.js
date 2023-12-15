a58391e87a37a73ac3e82a92ed7b299517696aa1
wild-railgun <wildrailgun@gmail.com>
2023-12-15 14:05:29 +0500

    A lot of Changes

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-  }
+}
+
+const questionBox = document.getElementsByClassName("question_box");
+
+for (let i = 0; i < questionBox.length; i++) {
+  questionBox[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
